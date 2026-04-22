6d0894d3ba92ab0b2d64ec7fa9b81f816f62e54c
bastacode21 <zaraku123456@gmail.com>
2026-04-22 11:21:38 +0800

    Add files via upload

---
+++ Temp/script.js
@@ -0,0 +1,26 @@
+const btn = document.querySelector("#addbtn");
+const counter = document.querySelector("#countertxt");
+const message = document.querySelector("#messagetxt");
+const reset = document.querySelector("#resetbtn");
+let timesclick = 0;
+btn.addEventListener("click", function () {
+  timesclick++;
+  console.log();
+  counter.innerHTML = timesclick;
+  if (timesclick <= 5) {
+    message.innerHTML = "Less than or equal to Five";
+  } else if (timesclick >= 6 && timesclick <= 10) {
+    message.innerHTML = "Less than or equal to Ten";
+  } else if (timesclick >= 11 && timesclick <= 15) {
+    message.innerHTML = "More Than Ten";
+  } else {
+    message.innerHTML = "Maximum Click Reached";
+  }
+});
+reset.addEventListener("click", function() {
+  timesclick = 0;
+  console.log();
+  counter.innerHTML = timesclick;
+  message.innerHTML = "Reseted Successfully"
+  
+})
